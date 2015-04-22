7ad16aaae57700ed6cc1dc81012d01c7336a6bcb
biauddar <biauddar@hotmail.com>
2015-04-22 06:12:51 +0000

    add pratice

---
+++ pratice.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo'test'.'<br>';
+    print'test'.'<br>';
+    echo'price:',(300*1.08),'yen'.'<br>';
+    print('test');
+    
+    ?>
+</body>
+</html>
