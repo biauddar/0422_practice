97110fba1d0b07ad36b8ff178c12ed62bfaadb1d
biauddar <biauddar@hotmail.com>
2015-04-22 05:35:58 +0000

    add sample03

---
+++ sample03.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print('now'.date33(' g:i:s'));
+    ?>
+    
+</body>
+</html>
